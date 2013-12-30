724c175697cfd4c57d21fca5126d7d956ece5629
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-12-30 20:10:36 +0530

    Muliple Routes

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 /*!
-** Example App
+** ngRouter App
 ** Licensed under the Apache License v2.0
 ** http://www.apache.org/licenses/LICENSE-2.0
 ** Built by Jay Kanakiya ( @techiejayk )
@@ -9,11 +9,29 @@
 
 var App = angular.module("example",["ngRoute"]);
 
-App.controller("ExmpCtrl",function  ($scope) {
-	$scope.message = "Hello World";
+App.controller("ExmpCtrl",function  ($scope,$route) {
+	$scope.message = "Takes it from Parent";
+	$scope.debug = $route;
+});
+
+App.controller("ChildCtrl1",function  ($scope) {
+	$scope.message = "Link 1 has been Visited";
+});
+
+App.controller("ChildCtrl2",function  ($scope) {
+	$scope.message = "Link 2 has been Visited";
 });
 
 App.config(function  ($routeProvider,$locationProvider) {
-	$routeProvider.when("/hello",{ templateUrl : "js/hello.html"});
+	$routeProvider
+	.when("/link1",{
+		templateUrl : "js/hello.html" , controller : "ChildCtrl1"
+	})
+	.when("/link2",{
+		templateUrl : "js/hello2.html" , controller : "ChildCtrl2"
+	})
+	.otherwise({
+		templateUrl : "js/hello3.html"
+	});
 	$locationProvider.html5Mode(true);
 });
