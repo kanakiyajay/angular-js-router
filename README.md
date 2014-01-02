47b736c594ad93c7f051211ca179335add4219d6
Jay Kanakiya <kanakiyajay@yahoo.com>
2014-01-02 16:15:47 +0530

    Step 4 using

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 The definative guide to Angular-js Router.
 
 Read at http://blog.jaykanakiya.com/getting-started-angular-js-router/
+
+![Angular-js Router](/img/angular-js%20router.jpg)
+
+*http://stackoverflow.com/questions/11541695/angular-js-redirecting-to-a-certain-route-based-on-condition
+*http://bradmcalister.com/routing-based-on-variables-in-angularjs/
+*http://arthur.gonigberg.com/2013/06/29/angularjs-role-based-auth/
+*http://stackoverflow.com/questions/16316087/angular-dynamic-templateurl
--- js/app.js
+++ js/app.js
@@ -9,36 +9,65 @@
 
 var App = angular.module("example",["ngRoute"]);
 
-App.controller("ExmpCtrl",function  ($scope,$route) {
+App.controller("ExmpCtrl",function  ($scope,$route,$location) {
 	$scope.message = "Takes it from Parent";
 	$scope.debug = $route;
 });
 
-App.controller("ChildCtrl1",function  ($scope,$routeParams) {
+App.run(function($rootScope, $location) {
+    // register listener to watch route changes
+    $rootScope.$on( "$locationChangeStart", function(event, next, current) {
+      if ( $rootScope.loggedUser == null ) {
+        // no logged user, we should be going to #login
+        if ( next.$$route.templateUrl == "js/hello.html" ) {
+          // already going to #login, no redirect needed
+        } else {
+          // not going to #login, we should redirect now
+          $location.path( "/" );
+        }
+      }
+    });
+ });
+
+App.controller("ChildCtrl1",function  ($scope,$route) {
 	$scope.message = "Link 1 has been Visited";
-	$scope.debug = $routeParams;
+	$scope.debug = $route;
 });
 
-App.controller("ChildCtrl2",function  ($scope,$routeParams) {
+App.controller("ChildCtrl2",function  ($scope,$route) {
 	$scope.message = "Link 2 has been Visited";
-	$scope.debug = $routeParams;
-});
-
-App.controller("ChildCtrl3",function  ($scope,$routeParams) {
-	$scope.message = "Link 3 has been visited";
-	$scope.debug = $routeParams;
+	$scope.debug = $route;
 });
 
 App.config(function  ($routeProvider,$locationProvider) {
 	$routeProvider
 	.when("/link1",{
-		templateUrl : "js/hello.html" , controller : "ChildCtrl1"
+		templateUrl : "js/hello.html" ,
+		controller : "ChildCtrl1"
 	})
-	.when("/link2/:linkId",{
-		templateUrl : "js/hello2.html" , controller : "ChildCtrl2"
+	.when("/link2",{
+		templateUrl : "js/hello2.html",
+		controller : "ChildCtrl2"
 	})
 	.otherwise({
 		templateUrl : "js/hello3.html"
 	});
-	$locationProvider.html5Mode(true);
+	$locationProvider.html5Mode(true);//required to work in plnkr
 });
+
+/*
+resolve - {Object.<string, function>=} - An optional map of dependencies which should be injected into the controller.
+	If any of these dependencies are promises,the router will wait for them all to be resolved or one to be rejected
+	before the controller is instantiated.
+	If all the promises are resolved successfully, the values of the resolved promises
+	are injected and $routeChangeSuccess event is fired.
+	If any of the promises are rejected the $routeChangeError event is fired.
+	The map object is:
+		key – {string}: a name of a dependency to be injected into the controller.
+		factory - {string|function}: If string then it is an alias for a service.
+		Otherwise if function, then it is injected and the return value is treated as the dependency.
+		If the result is a promise, it is resolved before its value is injected into the controller.
+		Be aware that ngRoute.$routeParams will still refer to the previous route within these resolve functions.
+
+	Use $route.current.params to access the new route parameters, instead.
+*/
